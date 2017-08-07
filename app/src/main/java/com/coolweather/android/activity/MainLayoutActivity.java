7202962e35e764bb3527db52a6f7b709c6032f2c
Wayne <502048022@qq.com>
2017-08-07 14:50:32 +0800

    first commit.

---
+++ app/src/main/java/com/coolweather/android/activity/MainLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.coolweather.android.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.coolweather.android.R;
+
+public class MainLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
